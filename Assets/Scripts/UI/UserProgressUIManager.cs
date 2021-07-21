7cc91c4ac59190f9a7592fecfa47af276653dcd5
vlAsTT <estadio7@gmail.com>
2021-07-22 00:36:34 +0300

    Scripts at UI, Utility namespace refactored & missing documentation is added
    All scripts within UI & Utility namespaces:
    - UserProgressUIManager.cs
    - JSONReader.cs
    
    were added necessary comments, checks & missing XML tags

--- Assets/Scripts/UI/UserProgressUIManager.cs
+++ Assets/Scripts/UI/UserProgressUIManager.cs
@@ -1,49 +1,95 @@
-using System;
 using TMPro;
 using UnityEngine;
 
 namespace UI
 {
+    /// <summary>
+    /// UI Manager that handles Score, Streak & Game Over functionality
+    /// </summary>
     public class UserProgressUIManager : MonoBehaviour
     {
         #region Variables
 
-        [Header("Gameplay Elements")] 
+        /// <summary>
+        /// Reference to the score text object
+        /// </summary>
+        /// <seealso cref="TextMeshProUGUI"/>
+        [Header("Gameplay Elements")] [Tooltip("Reference to the score text object that displays the actual score")]
         [SerializeField] private TextMeshProUGUI scoreField;
 
+        /// <summary>
+        /// Reference to the streak text object
+        /// </summary>
+        [Tooltip("Reference to the streak text object that displays the actual streak")]
         [SerializeField] private TextMeshProUGUI streakField;
 
+        /// <summary>
+        /// Reference to the Game Over Menu Object
+        /// </summary>
         [Header("In-Game Menu Elements")] 
-        [SerializeField] private GameObject gameOverScreen;
+        [Tooltip("Reference to the Game Over Menu Object")] [SerializeField] private GameObject gameOverScreen;
+        
+        /// <summary>
+        /// Reference to the Game Over score text object
+        /// </summary>
+        [Tooltip("Reference to the Game Over score text object that displays final score")]
         [SerializeField] private TextMeshProUGUI gameOverScoreField;
         
         #endregion
 
         #region Methods
 
+        /// <summary>
+        /// Initializes UI score & streak elements
+        /// </summary>
         private void Start()
         {
+            // UI Objects references validity checks
+            if (!scoreField || !streakField || !gameOverScreen || !gameOverScoreField)
+            {
+                Debug.LogError($"UI Reference(s) is/are missing at {name}");
+                return;
+            }
+            
+            // Hides Game Over Screen
             gameOverScreen.SetActive(false);
             
+            // Initializes Score & Streak UI values
             UpdateScore(0);
             UpdateStreak(1);
         }
 
+        #region Update / Show Values
+
+        /// <summary>
+        /// Sets new score at UI
+        /// </summary>
+        /// <param name="score">New Score</param>
         public void UpdateScore(int score)
         {
             scoreField.text = score.ToString();
         }
 
+        /// <summary>
+        /// Sets new streak at UI
+        /// </summary>
+        /// <param name="streak">New Streak</param>
         public void UpdateStreak(int streak)
         {
             streakField.text = streak.ToString();
         }
 
+        /// <summary>
+        /// Displays Game Over Screen with Final Score
+        /// </summary>
+        /// <param name="score">Final Score</param>
         public void ShowGameOverScreen(int score)
         {
             gameOverScreen.SetActive(true);
             gameOverScoreField.text = "Your Score:\n" + score;
         }
+
+        #endregion
 
         #endregion
     }
--- Assets/Scripts/Utility/JSONReader.cs
+++ Assets/Scripts/Utility/JSONReader.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 namespace Utility
 {
     /// <summary>
-    /// 
+    /// Set of methods that reads from JSON files and returns appropriate information parsed & ready-to-use
     /// </summary>
     public static class JSONReader
     {
+        /// <summary>
+        /// Parses JSON file and returns all items retrieved from there
+        /// </summary>
+        /// <param name="jsonFile">JSON Items File</param>
+        /// <returns>List of items obtained from parsing a JSON file</returns>
+        /// <seealso cref="ItemsDB"/>
         public static ItemsDB ReadItemsFromJson(TextAsset jsonFile)
         {
             ItemsDB itemsJson = JsonConvert.DeserializeObject<ItemsDB>(jsonFile.text);
